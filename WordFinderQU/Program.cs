56e15e087b8674f42c1dabf475c2ab28c5074001
Stenio Joseph <steniojoseph@hotmail.com>
2024-01-15 14:44:36 -0400

    Add Solution, Project to the repo

---
+++ WordFinderQU/Program.cs
@@ -0,0 +1,35 @@
+﻿using Game;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var matrix = new List<string>
+        {
+            "abcd",
+            "efgh",
+            "ijkl",
+            "mnop"
+        };
+
+        var wordstream = new List<string>
+        {
+            "ab",
+            "efg",
+            "hij",
+            "klm",
+            "nop",
+            "abc",
+            "mnop",
+            "ijkl"
+        };
+
+        WordFinder wordFinder = new WordFinder(matrix);
+
+        IEnumerable<string> foundWords = wordFinder.Find(wordstream);
+        foreach (var word in foundWords)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
---
+++ WordFinderQU/WordFinder.cs
@@ -0,0 +1,53 @@
+﻿namespace Game
+{
+    public class WordFinder
+    {
+        private readonly List<string> matrix;
+        private readonly List<string> transposedMatrix;
+
+        public WordFinder(IEnumerable<string> matrix)
+        {
+            this.matrix = matrix.ToList();
+            transposedMatrix = TransposedMatrix(matrix);
+        }
+
+        public IEnumerable<string> Find(IEnumerable<string> wordstream)
+        {
+            var wordCount = wordstream.GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
+
+            var uniqueWords = wordCount.Keys;
+            var foundWords = new Dictionary<string, int>();
+            foreach(var word in uniqueWords)
+            {
+                if(matrix.Any(row => row.Contains(word)) || transposedMatrix.Any(col => col.Contains(word)))
+                {
+                    foundWords.Add(word, wordCount[word]);
+                }
+            }
+            return foundWords.OrderByDescending(pair => pair.Value)
+                .Take(10)
+                .Select(pair => pair.Key);
+        }
+
+        private List<string> TransposedMatrix(IEnumerable<string> matrix)
+        {
+            var transposed = new List<string>();
+            if (!matrix.Any()) return transposed;
+
+            int rows = matrix.Count();
+            int cols = matrix.First().Length;
+
+            for(int col = 0; col < cols; col++)
+            {
+                char[] colChars = new char[rows];
+                int rowIndex = 0;
+                foreach(var row in matrix)
+                {
+                    colChars[rowIndex++] = row[col];
+                }
+                transposed.Add(new string(colChars));
+            }
+            return transposed;
+        }
+    }
+}
